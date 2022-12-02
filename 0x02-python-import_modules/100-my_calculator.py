f2132be2b2f51877c37e2740e1f6298cd159a1fe
Odu, Odu <theoduemmanuel@gmail.com>
2022-12-02 03:05:52 +0000

    * is not working

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     if n != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
+        exit(1)
     else:
         a = int(sys.argv[1])
         operator = sys.argv[2]
@@ -22,4 +22,4 @@ if __name__ == "__main__":
             print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-            exit (1)
+            exit(1)
