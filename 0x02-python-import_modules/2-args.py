401f5b57b4fff28b675d9d5013e250b0ec90d818
Odu, Odu <theoduemmanuel@gmail.com>
2022-12-02 00:57:54 +0000

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    n = len(sys.argv)
+
+    if n == 2:
+        for i in range(1, n):
+            print("{:d} argument:\n{:d}: {:s}".format(i, i, sys.argv[i]))
+    elif n > 2:
+        print("{:d} arguments:".format(n-1))
+        for i in range(1, n):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
+    else:
+        print("{:d} arguments.".format(0))
