f17cedde1b7aee6ed2b03392dbf450fa4863bd99
Odu, Odu <theoduemmanuel@gmail.com>
2022-12-06 01:19:22 +0000

    no max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    my_list.sort()
+    return (my_list[-1])
+
+
+"""
+    mx = my_list[0]
+    for i in my_list:
+        if i > mx:
+            mx = i
+    return mx"""
