60a38de961116a29b1cb123ee5270215c5aebc80
Odu, Odu <theoduemmanuel@gmail.com>
2022-12-11 04:18:37 +0000

    sets and dictionaries

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,12 @@
 def no_c(my_string):
     newStr = ""
     for i in my_string:
-        if i != 'c' and i != 'C':
+        if i not in 'cC':
             newStr += i
     return newStr
+    """
+    if not my_string:
+        pass
+    else:
+        return ''.join([char if char not in 'cC' else '' for char in my_string])
+        """
