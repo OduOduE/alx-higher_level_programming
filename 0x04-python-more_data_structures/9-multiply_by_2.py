82fac9ad674347e620092498b37a49f948a17df1
Odu, Odu <theoduemmanuel@gmail.com>
2022-12-11 04:51:44 +0000

    alll

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-def best_score(my_dict):
-    if my_dict is None or my_dict == {}:
-        return None
-    biggest = max(my_dict.values())
-    for key, value in my_dict.items():
-        if value is biggest:
-            return key
+
+def multiply_by_2(a_dictionary):
+    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
