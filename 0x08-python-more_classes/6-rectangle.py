f373df40e10b6556abdb7b2f6655077016b652b0
Odu, Odu <theoduemmanuel@gmail.com>
2023-01-03 07:16:12 +0000

    d

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """Represent a rectangle."""
 
-    number_of_instance = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
