6c454aaa5492409b00ce6bc8edf75ae7b1483edb
Odu, Odu <theoduemmanuel@gmail.com>
2023-01-26 02:56:27 +0000

    g

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,9 +8,12 @@ def say_my_name(first_name, last_name=""):
     Args:
         first_name (str): Firstname.
         last_name (str): Lastname.
-    Raise:
+    Returns:
+        No return.
+    Raises:
         TypeError if first_name or last_name is not a string.
     """
+
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
