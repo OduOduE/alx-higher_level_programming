72aff6ab8709128c361b8a9ecebe0eb442905000
OduOduE <theoduemmanuel@gmail.com>
2023-04-02 21:13:30 +0000

    f

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     import sys
     from urllib import request, error
 
-     try:
+    try:
         with request.urlopen(sys.argv[1]) as res:
             print(res.read().decode('UTF-8'))
     except error.HTTPError as er:
