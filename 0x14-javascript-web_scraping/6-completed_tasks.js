7f797291f7ee4438c4251838734b1d4654729b88
OduOduE <theoduemmanuel@gmail.com>
2023-04-25 12:02:15 +0000

    mandatoryDone

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,7 +4,7 @@ const request = require('request');
 
 const url = process.argv[2];
 
-request(url, function(err, response, body) {
+request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
